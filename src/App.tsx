498d1aa5311f184dea5f93f60360df729e162302
xiaoyuanlv <hello.xiaoyuanlv@gmail.com>
2022-08-31 20:09:19 +0630

    Update vertical scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainPage from './pages/MainPage/MainPage';
-import AboutPage from './pages/AboutPage/AboutPage';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage/PrivacyPolicyPage';
-import TermsPage from './pages/TermsPage/TermsPage';
-import DhammaListPage from './pages/DhammaListPage/DhammaListPage';
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage/PrivacyPolicyPage";
+import TermsPage from "./pages/TermsPage/TermsPage";
+import DhammaListPage from "./pages/DhammaListPage/DhammaListPage";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const App: React.FC = () => {
   return (
-    <>
-       <BrowserRouter>
+   
+      <BrowserRouter>
+       <ScrollToTop>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/dhamma/:id" element={<DhammaListPage />} />
@@ -26,9 +28,10 @@ const App: React.FC = () => {
             }
           />
         </Routes>
-        </BrowserRouter>
-    </>
-  )
-}
+        </ScrollToTop>
+      </BrowserRouter>
+   
+  );
+};
 
 export default App;
